669bbb31a6803e7901e7ddb5a1c1a59fe9110ad9
Nouman Fatta <noumanfatta@gmail.com>
2022-01-02 02:26:50 +0500

    comments added

--- src/Componenets/Cards.js
+++ src/Componenets/Cards.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext, useState, useEffect, useRef } from 'react';
 import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
@@ -15,8 +15,13 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { UserDetails } from './Profile';
 import { useSelector } from "react-redux";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 import { db } from "../firebase-config";
 import { doc, updateDoc, arrayUnion, arrayRemove } from "firebase/firestore";
+import { collection, setDoc, onSnapshot, getDoc, getDocs, orderBy, query } from 'firebase/firestore';
+import { Link } from 'react-router-dom';
+
 
 const ExpandMore = styled((props) => {
     const { expand, ...other } = props;
@@ -34,14 +39,53 @@ export default function Cards(props) {
     const [expanded, setExpanded] = useState(false);
     const currentUserDetails = useSelector(state => state.user.user);
     const [isLiked, setIsLiked] = useState(false);
-
+    const [comments, setComments] = useState([]);
+    const [commentName, setCommentName] = useState([])
+    const commentRef = useRef(null)
     useEffect(() => {
-        const likes = props.postdata.likes
-        setIsLiked(likes.includes(currentUserDetails.uid))
-    }, [currentUserDetails?.uid, props.postdata.likes])
+        const likes = props.postdata.likes;
+        setIsLiked(likes.includes(currentUserDetails.uid));
+        // getDocs(collection(db, "posts", props.postdata.postID, "comments"))
+        //     .then((querySnapshot) => {
+        //         querySnapshot.forEach((docs) => {
+        //             const nameRef = doc(db, "users", docs.data().commentedBy);
+        //             getDoc(nameRef).then((docSnap) => {
+        //                 setCommentName((prev) => [...prev, { comment: docs.data().comment, commentedBy: docSnap.data().firstName, commentedByID: docs.data().commentedBy }])
+        //             });
+        //         });
+        //     });
+
+        const docRef = query(collection(db, "posts", props.postdata.postID, "comments"), orderBy('commentedOn', 'desc'));
+        onSnapshot(docRef, (snapshot) => {
+            snapshot.docChanges().forEach((change) => {
+                if (change.type === "added") {
+                    const nameRef = doc(db, "users", change.doc.data().commentedBy);
+                    getDoc(nameRef).then((docSnap) => {
+                        setComments((prev) => [{ data: change.doc.data(), name: docSnap.data().firstName },...prev]);
+                    });
+                }
+            });
+        });
+
+    }, [currentUserDetails?.uid, props.postdata.likes, props.postdata.postID])
     const handleExpandClick = () => {
         setExpanded(!expanded);
     };
+
+    const commentNow = () => {
+        const docRef = doc(collection(db, "posts", props.postdata.postID, "comments"));
+        setDoc(docRef, {
+            commentID: docRef.id,
+            comment: commentRef.current.value,
+            commentedOn: new Date(),
+            commentedBy: currentUserDetails.uid
+        }).then(() => {
+            console.log("commented")
+        }).catch((error) => {
+            alert(error);
+        });
+    }
+
     const likePost = () => {
         if (isLiked) {
             const postRef = doc(db, "posts", props.postdata.postID);
@@ -89,11 +133,12 @@ export default function Cards(props) {
 
             <CardContent>
                 <Typography variant="body2" color="text.secondary">
-                {props.postdata.likes.length}
+                    {props.postdata.likes.length}
                 </Typography>
             </CardContent>
             <CardActions disableSpacing>
-                <IconButton onClick={(e) => likePost(e)} aria-label="add to favorites">
+
+                <IconButton onClick={likePost} aria-label="add to favorites">
                     <FavoriteIcon color={isLiked ? 'warning' : ''} />
                 </IconButton>
                 <IconButton aria-label="share">
@@ -113,30 +158,31 @@ export default function Cards(props) {
                     <Typography paragraph>
                         {props.postdata.description}
                     </Typography>
-                    <Typography paragraph>
-                        Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-                        aside for 10 minutes.
-                    </Typography>
-                    <Typography paragraph>
-                        Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-                        medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-                        occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
-                        large plate and set aside, leaving chicken and chorizo in the pan. Add
-                        pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
-                        stirring often until thickened and fragrant, about 10 minutes. Add
-                        saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-                    </Typography>
-                    <Typography paragraph>
-                        Add rice and stir very gently to distribute. Top with artichokes and
-                        peppers, and cook without stirring, until most of the liquid is absorbed,
-                        15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-                        mussels, tucking them down into the rice, and cook again without
-                        stirring, until mussels have opened and rice is just tender, 5 to 7
-                        minutes more. (Discard any mussels that don’t open.)
-                    </Typography>
-                    <Typography>
-                        Set aside off of the heat to let rest for 10 minutes, and then serve.
-                    </Typography>
+                    <TextField
+                        autoComplete="off"
+                        helperText=" "
+                        label="Comment"
+                        inputRef={commentRef}
+                    />
+                    <Button
+                        onClick={commentNow}
+                        variant="contained"
+                    >
+                        Comment
+                    </Button>
+                    {comments.map((val, ind) => {
+                        return (<p key={ind}> <span> {val.name}- </span> {val.data.comment} </p>)
+                    })
+                    }
+
+                    {/* {
+                        commentName.map((val, ind) => {
+                            return (
+                                <p key={ind}>  <span> <Link to={`/${val.commentedByID}`}> {val.commentedBy} </Link> - </span> {val.comment}  </p>
+                            )
+                        })
+                    } */}
+
                 </CardContent>
             </Collapse>
         </Card>
